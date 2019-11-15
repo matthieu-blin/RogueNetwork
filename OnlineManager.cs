43ba4882be28f8bf3393de96c2859976d5bc99f0
matthieu-blin <matthieu.blin@wildsheepstudio.fr>
2019-11-15 13:23:47 +0100

    basic msg handling

---
+++ OnlineManager.cs
@@ -0,0 +1,138 @@
+﻿using Assets;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using UnityEngine;
+
+public class OnlineManager : MonoBehaviour
+{
+    private static OnlineManager instance = null;
+    public static OnlineManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+
+    public string HostIP = "127.0.0.1";
+    public int HostPort = 25000;
+
+    bool m_Connected = false;
+    bool m_host = false;
+
+    public delegate void GameMessageCallback(byte[] _msg);
+
+    Dictionary<byte,GameMessageCallback> m_MessageCallbacksHandler;
+
+  
+    public void Awake()
+    {
+        instance = this;
+    }
+    public void Start()
+    {
+        m_tcp = new TCPApi();
+        m_tcp.Log = Log;
+        m_tcp.OnMessageReceived = OnGameMessage;
+        m_MessageCallbacksHandler = new Dictionary<byte, GameMessageCallback>();
+    }
+
+    private TCPApi m_tcp;
+
+    public void StartHost()
+    {
+        m_host = true;
+        m_tcp.Listen(HostIP, HostPort);
+        m_Connected = true;
+    }
+
+    public bool IsHost()
+    {
+        return m_host;
+    }
+
+    public List<EndPoint> m_clients = new List<EndPoint>();
+
+    public void StartClient()
+    {
+        m_Connected = m_tcp.Connect(HostIP, HostPort);
+    }
+
+    public bool IsConnected()
+    {
+        return m_Connected;
+    }
+
+    public void RegisterHandler(byte _handlerType, GameMessageCallback _cb)
+    {
+        GameMessageCallback cb;
+        if (m_MessageCallbacksHandler.TryGetValue(_handlerType, out cb))
+        {
+            m_MessageCallbacksHandler[_handlerType] = cb + _cb;
+        }
+        else
+        {
+            m_MessageCallbacksHandler.Add(_handlerType, _cb);
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsConnected())
+            return;
+        //clients management
+        m_clients.Clear();
+        m_tcp.FetchClients(m_clients);
+
+        m_tcp.Process();
+    }
+    public void Log(string txt)
+    {
+        Debug.Log(txt);
+    }
+    public void SendMessage(byte _handlerType, byte[] _msg)
+    {
+        using (MemoryStream m = new MemoryStream())
+        {
+            using (BinaryWriter w = new BinaryWriter(m))
+            {
+                w.Write(_handlerType);
+                w.Write(_msg.Length);
+                w.Write(_msg);
+                m_tcp.SendMessage(m.ToArray());
+            }
+        }
+        
+    }
+
+    public int OnGameMessage(TCPApi.Message msg)
+    {
+        using (MemoryStream m = new MemoryStream(msg.m_message))
+        {
+            using(BinaryReader r = new BinaryReader(m))
+            {
+                byte handlerType = r.ReadByte();
+                int size = r.ReadInt32();
+                byte[] buffer = r.ReadBytes(size);
+                GameMessageCallback cb;
+                if (m_MessageCallbacksHandler.TryGetValue(handlerType, out cb))
+                {
+                    cb(buffer);
+                }
+                else
+                {
+                    Log("Unhandled Message");
+                }
+            }
+        }
+        return 0;
+    }
+}
---
+++ TCPApi.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Runtime.Serialization.Formatters;
+using System.IO;
+using System.Security;
+
+namespace Assets
+{
+    public class TCPApi
+    {
+        public delegate void LogDelegate (string txt);
+        public delegate int MessageCallback(Message msg);
+
+        public LogDelegate Log;
+        public MessageCallback OnMessageReceived;
+
+        public static EndPoint ComputeEndPoint(string host, int port)
+        {
+            IPAddress ipAddress;
+            if (host.Length == 0)
+            {
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                ipAddress = ipHost.AddressList[0];
+            }
+            else
+            {
+                ipAddress = IPAddress.Parse(host);
+            }
+            return  new IPEndPoint(ipAddress , port);
+        }
+
+        public class Message
+        {
+            public uint m_playerID = 0;
+            public byte[] m_message = null;
+        }
+
+        protected readonly Object lockMessage = new Object();
+        protected List<Message> m_pendingMessages = new List<Message>();
+
+        public void Process()
+        {
+            lock (lockMessage)
+            {
+                foreach(Message msg in m_pendingMessages)
+                {
+                    OnMessageReceived(msg);
+                }
+                m_pendingMessages.Clear();
+            }
+        }
+
+        public const uint BufferSize = 4096;
+        class Client
+        {
+            public bool m_ListenClient = false;
+            public Thread m_Thread = null;
+            public Socket m_socket = null;
+        }
+
+        private readonly Object lockClients = new Object();
+        List<Client> m_clients = new List<Client>();
+        public TCPApi() { }
+
+        private Client NewClient(Socket _socket = null)
+        {
+            Client client = new Client();
+            if (_socket == null)
+                client.m_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            else
+                client.m_socket = _socket;
+            return client;
+        }
+
+        public void FetchClients(List<EndPoint> _clients)
+        {
+            lock(lockClients)
+            {
+                foreach (var client in m_clients)
+                {
+                    _clients.Add(client.m_socket.RemoteEndPoint);
+                }
+            }
+
+        }
+
+        private bool BindClient(ref Client _client, string localHost = "" , int localPort = 0)
+        {
+            //bind the socket 
+            try
+            {
+                EndPoint localEndPoint = ComputeEndPoint(localHost, localPort);
+                _client.m_socket.Bind(localEndPoint);
+            }
+            catch(ArgumentException )
+            {
+                Log("argument null");
+                return false;
+            }
+            catch(SocketException ex )
+            {
+                Log("Socket error " + ex.ToString());
+                return false;
+            }
+            catch(ObjectDisposedException )
+            {
+                Log("Socket closed ");
+                return false;
+            }
+            catch(SecurityException )
+            {
+                Log("Permission not granted");
+                return false;
+            }
+            return true;
+        }
+
+        private bool ConnectClient(ref Client _client, string localHost, int localPort)
+        {
+            EndPoint remoteEndPoint = ComputeEndPoint(localHost, localPort);
+            try
+            {
+                _client.m_socket.Connect(remoteEndPoint);
+            }
+            catch (ArgumentNullException)
+            {
+                Log("Wrong argument");
+                return false;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Log("invalid Port");
+                return false;
+            }
+            catch (SocketException ex)
+            {
+                Log("Socket error " + ex.ToString());
+                return false;
+            }
+            catch (ObjectDisposedException)
+            {
+                Log("Socket closed");
+                return false;
+            }
+            catch (NotSupportedException)
+            {
+                Log("Socket must be of IP or IPV6 type");
+                return false;
+            }
+            catch (InvalidOperationException)
+            {
+                Log("Socket must be in Listen state");
+                return false;
+            }
+            return true;
+        }
+
+
+        private Socket AcceptClient(ref Client client)
+        {
+            try
+            {
+                return client.m_socket.Accept();
+            }
+            catch (System.ObjectDisposedException ) // socket closed
+            {
+                return null;
+            }
+            catch (SocketException ex)
+            {
+                Log("Socket error " + ex.ToString());
+                return null;
+            }
+        }
+
+        //will start a Host client 
+        //Host client will wait connection on given parameters and will assign ID to new Client
+        public bool Listen(string localHost = "", int localPort = 0)
+        {
+            Log("Listening on" + localHost + ":" + localPort);
+            Client client = NewClient();
+            if (!BindClient(ref client, localHost, localPort))
+            {
+                Log("Cant start listening");
+                return false;
+            }
+            client.m_ListenClient = true;
+            //start listening for connection
+            client.m_socket.Listen(200);
+            //start accepting client thread
+            client.m_Thread = new Thread(() =>
+            {
+               while (true)
+               {
+                    Socket newSocket = AcceptClient(ref client);
+                    Client newClient = NewClient(newSocket);
+                    StartReceivingClient(newClient);
+                    lock (lockClients)
+                    {
+                        m_clients.Add(newClient);
+                    }
+                }
+            });
+            client.m_Thread.Start();
+            lock (lockClients)
+            {
+                m_clients.Add(client);
+            }
+            return true;
+
+        }
+
+        private void StartReceivingClient(Client client)
+        {
+            client.m_Thread = new Thread(() =>
+            {
+                //accept new client - blocking function
+                byte[] bytes = new byte[BufferSize];
+                while (true)
+                {
+                    //blocking function
+                    int numByte = client.m_socket.Receive(bytes);
+                    if (numByte == 4096)
+                    {
+                        Log("error : buffer size exceeded");
+                        break;
+                    }
+                    if (numByte == 0)
+                    {
+                        break;
+                    }
+                    Log("msg received with size " + numByte + " from " + client.m_socket.RemoteEndPoint.ToString());
+                    Message msg = new Message();
+                    msg.m_message = new byte[numByte];
+                    Array.Copy(bytes, msg.m_message, numByte);
+                    msg.m_playerID = 0;
+                    lock (m_pendingMessages)
+                    {
+                        m_pendingMessages.Add(msg);
+                    }
+                }
+            });
+            client.m_Thread.Start();
+        }
+
+        public bool Connect(string targetHost, int targetPort, string localHost = "", int localPort = 0 )
+        {
+            Client client = NewClient();
+            if(localHost.Length > 0)
+            {
+                if (!BindClient(ref client, localHost, localPort))
+                {
+                    Log("Cant connect to " + targetHost + ":" + targetPort);
+                    return false;
+                }
+            }
+            Log("Trying to connect to " + targetHost + ":" + targetPort);
+            if(!ConnectClient(ref client, targetHost, targetPort))
+            {
+                Log("Cant connect to " + targetHost + ":" + targetPort);
+                return false;
+            }
+            StartReceivingClient(client);
+            lock (lockClients)
+            {
+                m_clients.Add(client);
+            }
+            return true;
+        }
+
+        public int SendMessage(byte[] _msg)
+        {
+            if(_msg.Length > BufferSize)
+            {
+                Log("error : buffer size exceeded");
+                return -1;
+            }
+            int numBytes = 0;
+            lock (lockClients)
+            {
+                foreach (Client client in m_clients)
+                {
+                    if (!client.m_ListenClient)
+                        numBytes += client.m_socket.Send(_msg);
+                }
+            }
+            return numBytes;
+        }
+
+        public void End()
+        {
+            Log("Ending connection");
+            lock (lockClients)
+            {
+                foreach (Client client in m_clients)
+                {
+                    client.m_socket.Shutdown(SocketShutdown.Both);
+                    client.m_socket.Close();
+                    client.m_Thread.Join();
+                    client.m_socket = null;
+                    client.m_Thread = null;
+                }
+                m_clients.Clear();
+            }
+            Log("Connection ended");
+        }
+
+    }
+}
