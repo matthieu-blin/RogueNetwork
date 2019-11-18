9b89133ea8e9cc13f12633e73416b706d3982174
matthieu-blin <matthieu.blin@wildsheepstudio.fr>
2019-11-18 14:02:33 +0100

    skeleton of onlineObject

---
+++ OnlineIdentity.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlineIdentity : MonoBehaviour
+{
+
+    public ulong m_uid = 0;
+    public bool m_localAuthority = false;
+    public enum Type
+    { 
+        Static,
+        Dynamic,
+        HostOnly
+    };
+    public Type m_type = Type.Static;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch(m_type)
+        {
+            case Type.HostOnly:
+                {
+                    if (!OnlineManager.Instance.IsHost())
+                    {
+                        Destroy(this);
+                        return;
+                    }
+                    break;
+                }
+            case Type.Dynamic:
+                {
+                    if (!OnlineManager.Instance.IsHost())
+                    {
+                        Destroy(this);
+                        return;
+                    }
+                    else
+                    {
+                        OnlineObjectManager.Instance.Spawn(gameObject);
+                    }
+                    break;
+                }
+            case Type.Static:
+                {
+                    if (OnlineManager.Instance.IsHost())
+                    {
+                        OnlineObjectManager.Instance.ComputeID(gameObject);
+                    }
+                    break;
+                }
+
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ OnlineObjectManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlineObjectManager : MonoBehaviour
+{
+    private static OnlineObjectManager instance = null;
+    public static OnlineObjectManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    public void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn(GameObject _obj)
+    {
+
+    }
+    public void ComputeID(GameObject _obj) { }
+}
